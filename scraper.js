940e098cdef209376f76c87e9c7811f1b5db4ac6
Thomas Schuehly <thomas.schuehly@outlook.com>
2021-05-29 18:16:40 +0200

    First Version of Long Range Planning

--- scraper.js
+++ scraper.js
@@ -1,6 +1,10 @@
 const puppeteer = require('puppeteer');
 fs = require('fs');
-
+function delay(time) {
+    return new Promise(function(resolve) {
+        setTimeout(resolve, time)
+    });
+}
 (async () => {
 
     const browser = await puppeteer.launch({headless: false});
@@ -11,76 +15,84 @@ fs = require('fs');
     })
     page.setDefaultTimeout(60000)
 
-    const issueLinks = [];
-    for (let i = 2021; i > 1979; i--) {
-        await page.goto('https://onlinelibrary.wiley.com/loi/10970266/year/' + i)
-        try{
-            await page.waitForSelector('.parent-item');
-        }catch (e){
-            console.error("Error")
-        }
-        var yearLinks = await page.$$eval('.parent-item', li => {
-            return li.map(function (el){
-                    return el.getElementsByTagName('a')[0].href
-                }
-            )});
-        issueLinks.push.apply(issueLinks,yearLinks)
-    }
     let articles = [{}];
-    let authorObj;
-    for (const issuelink of issueLinks) {
-        await page.goto(issuelink)
-        let articlelink = issuelink.split('/')
-        let volArticles = []
-        let unfilteredArticles = []
-        unfilteredArticles = await page.$$eval('div.issue-item', div => {
-            return div.map(function (div) {
-                let article = {}
-                article.url = div.getElementsByTagName('a')[0].href
-                article.pageRange = div.getElementsByClassName('page-range')[0].lastChild.textContent
-                article.name = div.getElementsByTagName('h2')[0].textContent
-                return article
-            })
-        });
-        volArticles = unfilteredArticles.filter(function (art) {
-            return !(art.name === "Table of Contents"
-                || art.name === "Erratum"
-                || art.name === "Issue Information"
-                || art.name === "Masthead"
-                || art.name === "Copyright Page");
-        })
-        volArticles.map(article => {
-            article.id = articlelink[articlelink.length - 1]
-            article.volume = articlelink[articlelink.length - 2]
-            article.year = articlelink[articlelink.length - 3]
-        })
-        for (const article of volArticles) {
-            console.log("visiting: " + article.url)
-            await page.goto(article.url)
-            article.authors = []
-            const autInfo = await page.$$('.loa-authors .accordion-tabbed__tab-mobile')
-            for (const aut of autInfo) {
-                authorObj = {}
-                authorObj.name = await aut.$eval('a.author-name', name => name.textContent)
-                authorObj.origin = await aut.$eval('.author-info', el => {
-                    let p = el.getElementsByTagName('p')
-                    switch (p.length) {
-                        case 0:
-                            return ""
-                        case 1:
-                            return p[0].textContent
-                    }
-                    if (p.length > 1) {
-                        return p[1].textContent
-                    }
+    for (let i = 53; i > 0; i--) {
+        for (let j = 6; j > 0; j--){
+            let issuelink = 'https://www.sciencedirect.com/journal/long-range-planning/vol/'+ i + '/issue/' + j
+            await page.goto(issuelink)
+            try{
+                await page.waitForSelector('.js-article-list.article-list-items');
+                let unfilteredArticles = []
+                let volArticles = []
+                unfilteredArticles = await page.$$eval('dl.js-article', dl => {
+                    return dl.map(function (div) {
+                        let article = {}
+
+                        let h3 = div.querySelector('dt > h3')
+                        article.url = h3.getElementsByTagName('a')[0].href
+                        article.name = h3.getElementsByClassName('js-article-title')[0].textContent
+                        article.id = div.querySelector('.js-article-page-range').textContent
+                        return article
+                    })
+                });
+
+                volArticles = unfilteredArticles.filter(function (art) {
+                    return !(art.name === "Editorial Board"
+                        || art.name === "Erratum");
                 })
-                article.authors.push(authorObj)
+                volArticles.map(article => {
+                    article.issue = j
+                    article.volume = i
+                })
+
+                for (const article of volArticles) {
+                    console.log("visiting: " + article.url)
+                    await page.goto(article.url)
+                    article.authors = []
+                    await page.click('#show-more-btn')
+                    article.authors = await page.$$eval('#author-group', topDiv => {
+                        return topDiv.map(function (div) {
+                            let authorlist = []
+                            div.querySelectorAll('.author').forEach(
+                                function (div){
+                                let author = {}
+                                author.givenName = div.querySelector('.text.given-name').textContent
+                                author.surName = div.querySelector('.text.surname').textContent
+                                author.origin = [...div.querySelectorAll('.author-ref')].map(ref => ref.textContent)
+                                authorlist.push(author)
+                            })
+
+                            div.querySelectorAll('.affiliation').forEach(aff => {
+                                authorlist = authorlist.map(author => {
+                                    author.origin = author.origin.map(ref => {
+                                        if(ref === aff.querySelector('sup').textContent){
+                                            return aff.querySelector('dd').textContent
+                                        }else{
+                                            return ref
+                                        }
+                                    })
+                                    return author
+                                })
+
+                            })
+
+                            return authorlist
+                        })
+
+                    });
+                    console.log(JSON.stringify(article))
+                }
+                console.log(JSON.stringify(volArticles[0]))
+                articles.push.apply(articles, volArticles)
+            }catch (e){
+                console.error("Error: " + e)
             }
+
+            fs.writeFile('lrp_research_data.json', JSON.stringify(articles, null, 2), function (err) {
+                if (err) return console.log(err);
+                console.log(Date().toString() + ' : smj_research_data.json');
+            });
         }
-        articles.push.apply(articles, volArticles)
-        fs.writeFile('smj_research_data.json', JSON.stringify(articles, null, 2), function (err) {
-            if (err) return console.log(err);
-            console.log(Date().toString() + ' : smj_research_data.json');
-        });
     }
+
 })();
